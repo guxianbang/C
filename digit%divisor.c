885ed3eea0264ea77268d10cd201375a936848ce
guxianbnag <guxianbang@gmail.com>
2017-01-10 23:39:47 +0800

    修改

--- digit%divisor.c
+++ digit%divisor.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main(){
 	printf("2\n");
-	int digit;
-	int divisor;
-	int one;
+	int digit;//数字
+	int divisor;//除数
+	int one;//序号
 	one = 2;
 	for(digit = 3; digit <= 1000; digit += 2){
 		for(divisor = 3; divisor < digit; divisor += 2){
